57ee351cc16bac23cc72a656a8a08e11b38528e0
estefaniggt <estefanigabri@gmail.com>
2022-09-18 19:44:17 +0000

    Cree mi ramdom card, modifique el Style.css, el app.js y el index.html

--- src/app.js
+++ src/app.js
@@ -5,7 +5,29 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = () => {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pinta = generateRandomSimbol();
+
+  if (pinta == "♥" || pinta == "♦") {
+    document.querySelector(".PintaArriba").style.color = "red";
+    document.querySelector(".PintaAbajo").style.color = "red";
+  }
+
+  document.querySelector(".PintaArriba").innerHTML = pinta;
+  document.querySelector(".PintaAbajo").innerHTML = pinta;
+  document.querySelector(".Number").innerHTML = generateRandomNumber();
+};
+
+let generateRandomNumber = () => {
+  let numbers = ["A", "2", "3", "4", "5", "6", "7", "9", "10", "J", "Q", "K"];
+  let indexNumbers = Math.floor(Math.random() * numbers.length);
+  return numbers[indexNumbers];
+};
+
+let generateRandomSimbol = () => {
+  let simbol = ["♦", "♠", "♥", "♣"];
+
+  let valueSimbol = Math.floor(Math.random() * simbol.length);
+  return simbol[valueSimbol];
 };
